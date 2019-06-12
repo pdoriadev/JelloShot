5f68a7d0daf08fff3c21d4ab932dab96f804ebcd
Pitabreadgamedev <peterdcsulb16@gmail.com>
2019-06-12 15:40:41 -0700

    Michael's Tween system

---
+++ JelloShotUnityProject/Assets/TweenManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // For List
+using System;
+
+public class TweenManager : LazySingleton<TweenManager>
+{
+    private List<Coroutine> currentTweens = new List<Coroutine>();
+
+    public static void StartTween(Action<float> tweenFunc, float duration, Action onComplete)
+    {
+        TweenManager.Instance.BeginTween(tweenFunc, duration, onComplete);
+    }
+
+    public void BeginTween(Action<float> tweenFunc, float duration, Action onComplete)
+    {
+        Coroutine tween = StartCoroutine(CoTween(tweenFunc, duration, onComplete));
+
+        //currentTweens.Add(tween); // Track?
+    }
+
+    private IEnumerator CoTween(Action<float> tweenFunc, float duration, Action onComplete)
+    {
+        float timer = 0f;
+        while(timer < duration)
+        {
+            timer += Time.deltaTime;
+
+            if (tweenFunc != null)
+                tweenFunc(timer / duration);
+
+            //yield return new WaitForFixedUpdate();
+            yield return null;
+        }
+        if(onComplete != null)
+        {
+            onComplete();
+        }
+
+    }
+
+    private void Update()
+    {
+        //foreach (var tween in currentTweens)
+        //{
+        //    // Clear Finished
+        //    //if(tween.)
+        //}
+    }
+
+}
---
+++ JelloShotUnityProject/Assets/TweenTester.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TweenTester : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        SpriteRenderer sRend = GetComponent<SpriteRenderer>();
+        TweenManager.StartTween(
+            (float lerp) => {
+                transform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * 5f, lerp);
+                sRend.color = Color.Lerp(Color.white, Color.red, lerp);
+            },
+            1f,
+            () => {
+                Debug.Log("Completed Size Lerp");
+                TweenManager.StartTween(
+                    (float lerp) => {
+                    transform.localScale = Vector3.Lerp(Vector3.one*5f, Vector3.one*0.5f, lerp);
+                    sRend.color = Color.Lerp(Color.red, Color.blue, lerp);
+                    }, 
+                    0.5f,
+                    null
+                ); 
+            }
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
