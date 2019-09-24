6023e57dba07ed631d484c68a09eaf6f2f935aef
Peter Doria <pdoriadev@gmail.com>
2019-09-24 14:40:25 -0700

    Implemented new line visual for slingshot Decreased camera shake intensities.

--- JelloShotUnityProject/Assets/_SCRIPTS/Player/SlingShotVisuals.cs
+++ JelloShotUnityProject/Assets/_SCRIPTS/Player/SlingShotVisuals.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 /// </summary>
 public class SlingShotVisuals : MonoBehaviour
 {
-    // Arrows rotating right around player
+    public LineRenderer lineRend;
+    // Arrows rotating right around player ball
     public Transform ballArrowTransform;
     // Arrows rotating at First touch
     public Transform arrowsAtFirstTouchTransform;
@@ -16,44 +17,70 @@ public class SlingShotVisuals : MonoBehaviour
     public float angleArrowIsPointing;
 
     [SerializeField]
-    SlingShotMechanic _slingShotInstance;
+    private SlingShotMechanic _SlingShotInstance;
     #region UNITY CALLBACKS
 
     private void OnEnable()
     {
-        _slingShotInstance.slingShotMovesEvent += MoveVisualsOnSlingMove;
-        _slingShotInstance.slingShotResetEvent += MoveVisualsOffScreen;
+        if (_SlingShotInstance == null)
+            GameObject.FindGameObjectWithTag("Player").GetComponent<SlingShotMechanic>();
+
+        _SlingShotInstance.slingShotStartEvent += OnSlingStartListener;
+        _SlingShotInstance.slingShotMovesEvent += OnSlingMoveListener;
+        _SlingShotInstance.slingShotResetEvent += OnSlingResetListener;
     }
 
     private void OnDisable()
     {
-        _slingShotInstance.slingShotMovesEvent -= MoveVisualsOnSlingMove;
-        _slingShotInstance.slingShotResetEvent -= MoveVisualsOffScreen;
+        _SlingShotInstance.slingShotStartEvent -= OnSlingStartListener;
+        _SlingShotInstance.slingShotMovesEvent -= OnSlingMoveListener;
+        _SlingShotInstance.slingShotResetEvent -= OnSlingResetListener;
     }
     #endregion 
 
-    private void MoveVisualsOnSlingMove(TouchInfo _touchInfo, SlingShotInfo _slingShotInfo)
+    [SerializeField]
+    private ParticleSystem _PSysOnStart;
+    private void OnSlingStartListener(TouchInfo _touchInfo, SlingShotInfo _slingShotInfo)
     {
-        // arrows that rotate around player. 
+        _PSysOnStart.Play();
+    }
+
+    // Moves sling visuals
+    private void OnSlingMoveListener(TouchInfo _touchInfo, SlingShotInfo _slingShotInfo)
+    {
+        // set position for gameObjects rotating about player
         ballArrowTransform.position = _slingShotInfo.slingerRigidbody.transform.position;
+
+
+        Vector3 PosOffset = _slingShotInfo.slingerRigidbody.transform.position - _touchInfo.firstTouchPos;
+        // line end points positions
+        lineRend.SetPosition(0, _slingShotInfo.slingerRigidbody.transform.position);
+        lineRend.SetPosition(lineRend.positionCount - 1, _touchInfo.lastTouchPos + PosOffset);
+        Vector3 lineEndsOffset = lineRend.GetPosition(0) - lineRend.GetPosition(lineRend.positionCount - 1);
+        // set each inner line point's position equidistant to each other with the correct offset.
+        for (int i = 1; i < lineRend.positionCount - 1; i++)
+        {
+            lineRend.SetPosition(i , (lineRend.GetPosition(lineRend.positionCount - 1) + (lineEndsOffset / lineRend.positionCount) * (i + 1)));
+        }
+
         // ballArrowTransform and arrowsAtFirstTouchPosition's angle of rotation
         angleArrowIsPointing = Vector3.Angle(Vector3.up, _slingShotInfo.shotVelocity);
 
-        // Need 2 statements to calculate angle because Vector3.Angle only goes up to 180. 
+        // Need 2 ifs to calculate angle because Vector3.Angle only goes up to 180. 
         // Does not calculate angle going in one direction. Takes shortest distance from either side. 
         // if lastTouchPosition is right of firstTouchPosition, the arrowTransform's rotation changes to match
         if (_touchInfo.lastTouchPos.x > _touchInfo.firstTouchPos.x)
         {
             ballArrowTransform.eulerAngles = new Vector3(0, 0, angleArrowIsPointing);
-            arrowsAtFirstTouchTransform.eulerAngles = new Vector3(0, 0, angleArrowIsPointing);
-            latestTouchVisualTransform.eulerAngles = new Vector3(0, 0, angleArrowIsPointing);
+            //arrowsAtFirstTouchTransform.eulerAngles = new Vector3(0, 0, angleArrowIsPointing);
+            //latestTouchVisualTransform.eulerAngles = new Vector3(0, 0, angleArrowIsPointing);
         }
 
         if (_touchInfo.lastTouchPos.x <= _touchInfo.firstTouchPos.x)
         {
             ballArrowTransform.eulerAngles = new Vector3(0, 0, 360 - angleArrowIsPointing);
-            arrowsAtFirstTouchTransform.eulerAngles = new Vector3(0, 0, 360 - angleArrowIsPointing);
-            latestTouchVisualTransform.eulerAngles = new Vector3(0, 0, 360 - angleArrowIsPointing);
+            //arrowsAtFirstTouchTransform.eulerAngles = new Vector3(0, 0, 360 - angleArrowIsPointing);
+            //latestTouchVisualTransform.eulerAngles = new Vector3(0, 0, 360 - angleArrowIsPointing);
         }
 
         // Moves finger touch visual to the latest touch position
@@ -62,10 +89,14 @@ public class SlingShotVisuals : MonoBehaviour
         arrowsAtFirstTouchTransform.position = _touchInfo.firstTouchPos;
     }
 
-    public void MoveVisualsOffScreen()
+    public void OnSlingResetListener()
     {
         ballArrowTransform.position = offScreenPosition;
         arrowsAtFirstTouchTransform.position = offScreenPosition;
         latestTouchVisualTransform.position = offScreenPosition;
+        for (int i = 0; i < lineRend.positionCount; i++)
+        {
+            lineRend.SetPosition(i, offScreenPosition);
+        }
     }
 }
